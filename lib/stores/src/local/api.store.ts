6f349c8f45f7754986040add4c4d7560d66c219c
Elias Mawa <elias@emawa.io>
2023-05-11 13:51:16 -0600

    Squashed commit of the following:
    commit bba538c9dc7925676e5e6cf604d640cbf1be94d6
    Author: Elias Mawa <elias@emawa.io>
    Date:   Thu May 11 13:50:17 2023 -0600
    
        fix: api store

--- lib/stores/src/local/api.store.ts
+++ lib/stores/src/local/api.store.ts
@@ -4,7 +4,6 @@ import { web_config } from "@lib/config";
 export const apiStore = () => {
   const stande = new Stande({
     base_url: web_config.api_host,
-    api_token: web_config.directus.public_api_token,
     api_token_type: "Bearer",
   });
 
--- lib/stores/src/local/content.store.ts
+++ lib/stores/src/local/content.store.ts
@@ -4,7 +4,6 @@ import { web_config } from "@lib/config";
 export const contentStore = () => {
   const stande = new Stande({
     base_url: web_config.cms_host,
-    api_token: web_config.directus.public_api_token,
     api_token_type: "Bearer",
   });
 
