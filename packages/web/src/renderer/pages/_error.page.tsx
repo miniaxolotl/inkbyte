33ba275244e5b1b18ba3fe9cb48d84ab7d813dfe
Elias Mawa <elias@emawa.io>
2023-05-11 14:03:06 -0600

    update: error page style

--- packages/web/src/renderer/pages/_error.page.tsx
+++ packages/web/src/renderer/pages/_error.page.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 
+import { Container } from "@mantine/core";
+
+import { LayoutDefault } from "@components/layout";
+
 export { Page };
 
 function Page({ is404 }: { is404: boolean }) {
   if (is404) {
     return (
-      <>
-        <h1>404 Page Not Found</h1>
-        <p>This page could not be found.</p>
-      </>
+      <LayoutDefault>
+        <Container sx={{ textAlign: "center" }}>
+          <h1>404 Page Not Found</h1>
+          <p>This page could not be found.</p>
+        </Container>
+      </LayoutDefault>
     );
   } else {
     return (
-      <>
-        <h1>500 Internal Server Error</h1>
-        <p>Something went wrong.</p>
-      </>
+      <LayoutDefault>
+        <Container sx={{ textAlign: "center" }}>
+          <h1>500 Internal Server Error</h1>
+          <p>Something went wrong.</p>
+        </Container>
+      </LayoutDefault>
     );
   }
 }
