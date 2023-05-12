f2d6c65c2f9f47cf360e9d9b8f85431470ca0c79
Elias Mawa <elias@emawa.io>
2023-05-12 12:47:11 -0600

    feature: text divider

---
+++ packages/web/src/components/core/TextDivider.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Box, BoxProps, Divider, Text } from "@mantine/core";
+import _ from "lodash";
+
+type TextDividerProps = Omit<BoxProps, "children"> & {
+  href?: string;
+  // children?: React.ReactNode;
+  children?: string;
+};
+
+export const TextDivider = (props: TextDividerProps) => {
+  return (
+    <Box
+      {..._.omit(props, ["size"])}
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        padding: "16px 8px",
+        margin: "auto",
+        ...props.sx,
+      }}
+    >
+      <Divider sx={{ flex: 1 }} />
+      <Text
+        weight={600}
+        sx={{
+          flex: 0,
+          padding: "0 12px",
+          fontFamily: "'Secular One', sans-serif",
+          "::after": { content: `'${props.children ?? "OR"}'` },
+        }}
+      >
+        {/* {props.children} */}
+      </Text>
+      <Divider sx={{ flex: 1 }} />
+    </Box>
+  );
+};
--- packages/web/src/components/core/index.ts
+++ packages/web/src/components/core/index.ts
@@ -1,1 +1,2 @@
+export * from "./TextDivider";
 export * from "./Link";
