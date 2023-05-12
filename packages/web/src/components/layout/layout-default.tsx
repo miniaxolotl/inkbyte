929cb124a16aa407a3038b4f16b3ce59fc2f1aee
Elias Mawa <elias@emawa.io>
2023-05-11 18:42:02 -0600

    update: layout & navigation

--- packages/web/src/components/layout/layout-default.tsx
+++ packages/web/src/components/layout/layout-default.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-import { Box, Title } from "@mantine/core";
-import { FiClock, FiLogIn, FiUserPlus } from "react-icons/fi/index.js";
+import { ActionIcon, Box, Drawer, Modal, Title } from "@mantine/core";
+import { FiMenu } from "react-icons/fi/index.js";
+import { useDisclosure } from "@mantine/hooks";
 
 import { web_config } from "@lib/config";
 
 import {
   DesktopNavigation,
   DesktopNavigationItem,
+  MobileNavigation,
+  MobileNavigationItem,
 } from "@components/layout/navigation";
 import { FooterDefault } from "@components/layout/footer";
 import { Link } from "@components/core";
@@ -18,6 +21,8 @@ export type LayoutDefaultProps = {
 };
 
 export const Content = ({ children }: { children: React.ReactNode }) => {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <Box
       sx={{
@@ -27,32 +32,83 @@ export const Content = ({ children }: { children: React.ReactNode }) => {
         height: "100%",
       }}
     >
-      <DesktopNavigation title={web_config.app_title} sx={{ padding: 16 }}>
+      <DesktopNavigation title={web_config.app_title} sx={{ padding: 24 }}>
         <DesktopNavigationItem>
           <Title size="h4" order={2}>
-            <Link href="/history">
-              <FiClock />
-              History
-            </Link>
+            <Link href="/features">Features</Link>
           </Title>
         </DesktopNavigationItem>
+
         <DesktopNavigationItem>
+          <Title size="h4" order={2}>
+            <Link href="/dashboard">Dashboard</Link>
+          </Title>
+        </DesktopNavigationItem>
+        {/* <DesktopNavigationItem>
+          <ActionIcon variant="filled" color="brand-blue">
+            <FiClock />
+          </ActionIcon>
+        </DesktopNavigationItem> */}
+        <DesktopNavigationItem>
+          <Title size="h4" order={2}>
+            <Link href="/login">Login</Link>
+          </Title>
+          /
+          <Title size="h4" order={2}>
+            <Link href="/register">Register</Link>
+          </Title>
+        </DesktopNavigationItem>
+        {/* <DesktopNavigationItem>
+          <Link href="/register">
+            <Button leftIcon={<FiUser />} color="brand-cerulean">
+              Register
+            </Button>
+          </Link>
+        </DesktopNavigationItem> */}
+        {/* <DesktopNavigationItem>
           <Title size="h4" order={2}>
             <Link href="/register">
               Register
               <FiUserPlus />
             </Link>
           </Title>
-        </DesktopNavigationItem>
-        <DesktopNavigationItem>
+        </DesktopNavigationItem> */}
+        {/* <DesktopNavigationItem>
           <Title size="h4" order={2}>
             <Link href="/login">
               Login
               <FiLogIn />
             </Link>
           </Title>
-        </DesktopNavigationItem>
+        </DesktopNavigationItem> */}
       </DesktopNavigation>
+
+      <MobileNavigation title={web_config.app_title} sx={{ padding: 24 }}>
+        <MobileNavigationItem>
+          <Drawer
+            opened={opened}
+            onClose={close}
+            title="Menu"
+            size="xs"
+            position="right"
+            overlayProps={{
+              opacity: 0.5,
+              blur: 4,
+            }}
+            sx={{
+              ".mantine-Drawer-header": { padding: 24 },
+              "	.mantine-Drawer-close": { width: 24, height: 24 },
+            }}
+          >
+            {/* Drawer content */}
+          </Drawer>
+
+          <ActionIcon size="xl" onClick={open}>
+            <FiMenu size={24} />
+          </ActionIcon>
+        </MobileNavigationItem>
+      </MobileNavigation>
+
       <Box sx={{ flex: 1, flexGrow: 1, padding: "16px 0" }}>{children}</Box>
       <FooterDefault sx={{ padding: 16 }} />
       <Waves />
