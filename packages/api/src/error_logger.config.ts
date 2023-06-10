54840d2241e3899efde4949f92b04cbf8c67128f
Elias Mawa <elias@emawa.io>
2023-06-09 20:02:35 -0600

    update: error logger

--- packages/api/src/error_logger.config.ts
+++ packages/api/src/error_logger.config.ts
@@ -1,11 +1,18 @@
 import { Next, ParameterizedContext } from "koa";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-import { api_config } from "@lib/config";
+import { CLIENT_ERROR } from "@lib/utility";
+// import { api_config } from "@lib/config";
 
 export const error_logger = async (ctx: ParameterizedContext, next: Next) => {
   try {
     return await next();
   } catch (e: unknown) {
-    if (api_config.print_errors) console.log(e as Error);
+    if (e instanceof PrismaClientKnownRequestError && e.code === "P2025") {
+      ctx.status = CLIENT_ERROR.NOT_FOUND.status;
+      ctx.body = CLIENT_ERROR.NOT_FOUND.message;
+    }
+
+    // if (api_config.print_errors) console.log(e as Error);
   }
 };
