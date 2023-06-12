6e37d5f19498ab963a072a8a70fc3f5d67cf3e1b
Elias Mawa <elias@mawa.dev>
2023-06-12 01:20:16 -0600

    update: config files

--- packages/web/.eslintrc.cjs
+++ packages/web/.eslintrc.cjs
@@ -10,16 +10,19 @@ module.exports = {
     project: ["./tsconfig.build.json"],
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react-hooks"],
   extends: [
+    "prettier",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier",
+    "plugin:react/recommended",
   ],
   rules: {
     "@typescript-eslint/no-explicit-any": "error",
     // "@typescript-eslint/sort-type-constituents": "error",
     "sort-imports": ["error"],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
   },
   settings: {
     react: {
