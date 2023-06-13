80c467dde1eaa86e4afdd3b4de65ba6ebc739a8f
Elias Mawa <elias@mawa.dev>
2023-06-13 07:01:24 -0600

    update: api services

--- lib/services/src/service/user.service.ts
+++ lib/services/src/service/user.service.ts
@@ -1,5 +1,7 @@
-import type { CreateUserSchema, UserSchema } from "@lib/schema-validator";
-import { PermissionRoleMap } from "@lib/shared";
+import { isFuture } from "date-fns";
+
+import { BaseUser, PermissionRoleMap } from "@lib/shared";
+import type { UserCreateSchema, UserSchema } from "@lib/schema-validator";
 import { genHash } from "@lib/crypt";
 import { gen_id } from "@lib/utility";
 import { prisma_db } from "@lib/database";
@@ -10,7 +12,16 @@ import { prisma_db } from "@lib/database";
 
 export const get_users = async () => {
   const user = await prisma_db.user.findMany({
-    include: { roles: { include: { role: true } } },
+    include: {
+      image: true,
+      roles: {
+        where: { deleted: null },
+        orderBy: { role: { authority: "desc" } },
+        include: {
+          role: true,
+        },
+      },
+    },
   });
   return user;
 }; // get_users
@@ -18,12 +29,38 @@ export const get_users = async () => {
 export const get_user_by_id = async (id: number) => {
   const user = await prisma_db.user.findUnique({
     where: { id },
-    include: { roles: { include: { role: true } } },
+    include: {
+      image: true,
+      roles: {
+        where: { deleted: null },
+        orderBy: { role: { authority: "desc" } },
+        include: {
+          role: true,
+        },
+      },
+    },
   });
   return user;
 }; // get_user_by_id
 
-export const create_user = async (payload: CreateUserSchema) => {
+export const get_user_by_email = async (email: string) => {
+  const user = await prisma_db.user.findUnique({
+    where: { email },
+    include: {
+      image: true,
+      roles: {
+        where: { deleted: null },
+        orderBy: { role: { authority: "desc" } },
+        include: {
+          role: true,
+        },
+      },
+    },
+  });
+  return user;
+}; // get_user_by_email
+
+export const create_user = async (payload: UserCreateSchema) => {
   const user = await prisma_db.user.create({
     data: {
       uid: gen_id(),
@@ -36,7 +73,7 @@ export const create_user = async (payload: CreateUserSchema) => {
       verification_date: new Date(),
       roles: {
         create: {
-          role_id: PermissionRoleMap.BASIC_USER.id,
+          role_id: PermissionRoleMap.basic_user.id,
           updated_by: 1,
         },
       },
@@ -56,7 +93,7 @@ export const patch_user = async (payload: UserSchema) => {
   return user;
 }; // patch_user
 
-export const patch_user_password = async (payload: CreateUserSchema) => {
+export const patch_user_password = async (payload: UserCreateSchema) => {
   const user = await prisma_db.user.update({
     where: { email: payload.email },
     data: { password: await genHash(payload.password) },
@@ -71,3 +108,20 @@ export const soft_delete_user = async (id: number) => {
   });
   return user;
 }; // soft_delete_user
+
+//***********************************************
+//* user utility
+//***********************************************
+
+export const update_user_last_ip = async (user_id: number, last_ip: string) => {
+  return await prisma_db.user.update({
+    where: { id: user_id },
+    data: { last_ip },
+  });
+}; // update_user_last_ip
+
+export const is_user_verified = (user: BaseUser) => {
+  if (!user.verification_token || !user.verification_date) return false;
+  if (isFuture(user.verification_date)) return false;
+  return true;
+}; // is_user_verified
