605d321e56c2b88842cc60413f2171272e883faa
Elias Mawa <elias@mawa.dev>
2023-06-13 07:02:08 -0600

    update: api output func

--- packages/api/src/sanitize.config.ts
+++ packages/api/src/sanitize.config.ts
@@ -6,7 +6,7 @@ export const sanitize = async (ctx: ParameterizedContext, next: Next) => {
   await next();
   ctx.body = exclude(ctx.body as BaseMap, {
     keys: ["id", "password"],
-    like: ["_id"],
+    like: ["_id", "_token", "verification_", "created", "deleted", "updated"],
   });
   if (typeof ctx.body === "string") ctx.set("Content-Type", "text/plain");
 };
