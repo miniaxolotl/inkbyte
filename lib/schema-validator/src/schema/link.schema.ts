6865ee17c75402a0e44d12ebc1148b6cfb06f019
Elias Mawa <elias@mawa.dev>
2023-06-13 07:02:48 -0600

    update: schema validatyor

--- lib/schema-validator/src/schema/link.schema.ts
+++ lib/schema-validator/src/schema/link.schema.ts
@@ -1,13 +1,13 @@
 import { InferType, object, string } from "yup";
 
 //***********************************************
-//* user
+//* link
 //***********************************************
 
-export const createLinkSchema = object({
+export const linkCreateSchema = object({
   long_url: string().label("Long URL").url().max(256).required(),
   domain: string().label("Domain").max(24).required(),
   custom_slug: string().label("Custom Alias").max(24),
 });
 
-export type CreateLinkSchema = InferType<typeof createLinkSchema>;
+export type LinkCreateSchema = InferType<typeof linkCreateSchema>;
