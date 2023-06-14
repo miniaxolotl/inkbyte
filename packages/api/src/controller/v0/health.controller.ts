ab36846ddbb42b305f8cd89c774680e2894ba54d
Elias Mawa <elias@mawa.dev>
2023-06-13 21:51:22 -0600

    feature: health check routes

--- packages/api/src/controller/v0/health.controller.ts
+++ packages/api/src/controller/v0/health.controller.ts
@@ -3,12 +3,12 @@ import { ParameterizedContext } from "koa";
 
 import { SUCCESS } from "@lib/utility";
 
-export const route = ["/ping"];
+export const route = ["/health"];
 export const router = new KoaRouter();
 
 export const PingController = { router, route };
 
 router.get("/", async (ctx: ParameterizedContext) => {
-  ctx.response.body = "Pong!";
+  ctx.response.body = "Hello World!";
   ctx.response.status = SUCCESS.OK.status;
 }); // {get} /v1/hello
