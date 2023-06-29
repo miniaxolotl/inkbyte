759910fa9da65e5c7d702ae16e0d1657df7725db
Elias Mawa <elias@mawa.dev>
2023-06-29 15:54:55 -0600

    update: ssr vercel features

--- packages/web/api/ssr.js
+++ packages/web/api/ssr.js
@@ -1,21 +1,52 @@
 import { renderPage } from "vite-plugin-ssr/server";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req, res) {
-  const { url } = req;
+/**
+ * @param {NextApiResponse} request
+ * @param {NextApiResponse} response
+ * @returns
+ */
+export default async function handler(request, response) {
+  const { url, originalUrl } = request;
+  const hostname = request.hostname || originalUrl || request.headers.host;
   console.log("Request to url:", url);
 
-  const pageContextInit = { url };
+  const pageContextInit = {
+    urlOriginal,
+    origin: hostname || "",
+    referer: hostname || "",
+    cookies: request.cookies,
+  };
+
+  if (!request.cookies.session_id) {
+    const session_id = `${new Date().getTime()}-${uuid()}`;
+    request.cookies.session_id = session_id;
+    response.cookie("session_id", session_id, {
+      path: "/",
+      expires: addDays(new Date(), 30),
+    });
+  }
+
   const pageContext = await renderPage(pageContextInit);
   const { httpResponse } = pageContext;
-
-  if (!httpResponse) {
-    res.statusCode = 200;
-    res.end();
+  if (pageContext.redirectTo) {
+    return response.redirect(302, pageContext.redirectTo);
+  } else if (!httpResponse) {
+    response.statusCode = 200;
+    response.end();
     return;
+  } else {
+    const { body, statusCode, contentType, earlyHints } = httpResponse;
+    if (response.writeEarlyHints) {
+      response.writeEarlyHints({
+        link: earlyHints.map((e) => e.earlyHintLink),
+      });
+    }
+    response.status(statusCode).type(contentType).send(body);
   }
 
   const { body, statusCode, contentType } = httpResponse;
-  res.statusCode = statusCode;
-  res.setHeader("content-type", contentType);
-  res.end(body);
+  response.statusCode = statusCode;
+  response.setHeader("content-type", contentType);
+  response.end(body);
 }
