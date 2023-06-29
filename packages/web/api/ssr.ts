9a16b62aa6888391bfa8185e555801c29bf9b37e
Elias Mawa <elias@mawa.dev>
2023-06-29 16:45:04 -0600

    fix: vercel func

--- packages/web/api/ssr.ts
+++ packages/web/api/ssr.ts
@@ -32,23 +32,11 @@ export default async function handler(
     return response.redirect(302, pageContext.redirectTo);
   } else if (!httpResponse) {
     response.statusCode = 200;
-    response.end();
-    return;
+    return response.end();
   } else {
-    const { body, statusCode, contentType, earlyHints } = httpResponse;
-    // if (response.writeEarlyHints) {
-    //   response.writeEarlyHints({
-    //     link: earlyHints.map((e) => e.earlyHintLink),
-    //   });
-    // }
-    response
-      .status(statusCode)
-      .setHeader("Content-Type", contentType)
-      .send(body);
+    const { body, statusCode, contentType } = httpResponse;
+    response.statusCode = statusCode;
+    response.setHeader("content-type", contentType);
+    response.end(body);
   }
-
-  const { body, statusCode, contentType } = httpResponse;
-  response.statusCode = statusCode;
-  response.setHeader("content-type", contentType);
-  response.end(body);
 }
